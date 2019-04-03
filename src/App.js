54b658834eddade94f9b486f4da6df1be5be4fe0
Bhavya <kshubhamm@gmail.com>
2019-04-03 18:40:09 +0530

    search added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Information from './info-json';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+  constructor(){
+    super();
+
+    this.state={
+      search:null
+    };
+  }
+
+  searchSpace=(event)=>{
+    let keyword = event.target.value;
+    this.setState({search:keyword})
+  }
+
+  render(){
+    const styleInfo = {
+      paddingRight:'10px'
+    }
+    const elementStyle ={
+      border:'solid',
+      borderRadius:'10px',
+      position:'relative',
+      left:'10vh',
+      height:'3vh',
+      width:'20vh',
+      marginTop:'5vh',
+      marginBottom:'10vh'
+    }
+    const items = Information.filter((data)=>{
+      if(this.state.search == null)
+          return data
+      else if(data.name.toLowerCase().includes(this.state.search.toLowerCase()) || data.country.toLowerCase().includes(this.state.search.toLowerCase())){
+          return data
+      }
+    }).map(data=>{
+      return(
+      <div>
+        <ul>
+          <li style={{position:'relative',left:'10vh'}}>
+            <span style={styleInfo}>{data.name}</span>
+            <span style={styleInfo}>{data.age}</span>
+            <span style={styleInfo}>{data.country}</span>
+          </li>
+        </ul>
       </div>
-    );
+      )
+    })
+
+    return (
+      <div>
+      <input type="text" placeholder="Enter item to be searched" style={elementStyle} onChange={(e)=>this.searchSpace(e)} />
+      {items}
+      </div>
+    )
   }
 }
 
---
+++ src/info-json.js
@@ -0,0 +1,61 @@
+import react,{Component} from "react";
+
+const Information = [
+  {
+    "name":"Samule",
+    "age":21,
+    "country":"USA"
+  },
+  {
+    "name":"Sam",
+    "age":21,
+    "country":"USA"
+  },
+  {
+    "name":"Mark",
+    "age":21,
+    "country":"Africa"
+  },
+  {
+    "name":"Markus",
+    "age":21,
+    "country":"Africa"
+  },
+  {
+    "name":"Aayush",
+    "age":21,
+    "country":"India"
+  },
+  {
+    "name":"Sean",
+    "age":21,
+    "country":"Ireland"
+  },
+  {
+    "name":"Eduardo",
+    "age":21,
+    "country":"France"
+  },
+  {
+    "name":"Dustin",
+    "age":21,
+    "country":"Spain"
+  },
+  {
+    "name":"Alexendra",
+    "age":21,
+    "country":"USA"
+  },
+  {
+    "name":"Lee",
+    "age":21,
+    "country":"China"
+  },
+  {
+    "name":"Jim",
+    "age":21,
+    "country":"Korea"
+  }
+];
+
+export default Information;
